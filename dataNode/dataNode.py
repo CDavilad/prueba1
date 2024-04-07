10742566742d91ebbdf154ce31c56bcce18a3202
CDavilad <63668850+CDavilad@users.noreply.github.com>
2024-04-07 12:01:56 -0500

    Update dataNode.py

--- dataNode/dataNode.py
+++ dataNode/dataNode.py
@@ -91,4 +91,4 @@ if __name__ == '__main__':
     #     return Response(contenido_archivo, mimetype='application/octet-stream')
 
 
-    app.run(debug=True, host=0.0.0.0, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
