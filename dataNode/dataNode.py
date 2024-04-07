f8588cd9fc2eac56f1b19ea49799d068cff3b34a
CDavilad <63668850+CDavilad@users.noreply.github.com>
2024-04-07 10:35:56 -0500

    Add files via upload

---
+++ dataNode/dataNode.py
@@ -0,0 +1,93 @@
+from io import BytesIO
+import sys
+import requests
+from flask import Flask, request, send_file,jsonify, Response
+import base64
+import socket
+
+def obtener_direccion_ip():
+    host = socket.gethostname()
+    direccion_ip = socket.gethostbyname(host)
+    return direccion_ip
+
+def registrar_con_servidor(host, port, capacidad):
+    server_url = 'http://127.0.0.1:5000/register'
+    data = {
+        'host': host,
+        'port': port,
+        'capacidad': capacidad
+    }
+    try:
+        response = requests.post(server_url, json=data)
+        if response.status_code == 200:
+            print("DataNode registrado correctamente en el servidor.")
+        else:
+            print("Error al registrar DataNode en el servidor.")
+    except requests.exceptions.RequestException as e:
+        print("Error al conectar con el servidor:", e)
+
+if __name__ == '__main__':
+    host = obtener_direccion_ip()
+    
+    # Obtener el puerto de la línea de comandos
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 8000
+
+    # Definir el límite de peso en bytes (1000 KB en este caso)
+    limite_peso_kilo_bytes = 500.0 # 500 KB 
+
+    # Inicializar la lista de archivos
+    archivos_guardados = {}
+
+    # Inicializar la lista de nombre de archivos
+    nombre_archivos = []
+
+
+    registrar_con_servidor(host, port, 500.0)
+
+    app = Flask(__name__)
+
+    @app.route('/guardar', methods=['POST'])
+    def guardar_archivo():
+        datos_archivo = request.json.get('archivo')
+        nombre_archivo = datos_archivo.get('nombre')
+        contenido_archivo = datos_archivo.get('archivo')
+        tamaño_archivo = datos_archivo.get('tamaño_archivo')
+
+        global limite_peso_kilo_bytes  # Declarar como global para modificar la variable global
+
+
+        capacidad_disponible = limite_peso_kilo_bytes - tamaño_archivo
+        limite_peso_kilo_bytes = capacidad_disponible
+
+        # Guardar el archivo en el diccionario con su nombre
+        archivos_guardados[nombre_archivo] = contenido_archivo
+        
+        requests.post(f'http://127.0.0.1:5000/actualizarCapacidadDataNode', json={'data': {'host': host, 'port': port, 'nuevaCapacidad': capacidad_disponible}})
+             
+        return f'Archivo guardado correctamente en el DataNode. Host: {host}, Puerto: {port}', 200
+    
+
+    @app.route('/recuperar_archivo', methods=['GET'])
+    def recuperar_archivo():
+        data = request.json.get('data_archivo')
+        nombre_archivo = data['nombre_archivo'] 
+
+
+        contenido_archivo = archivos_guardados[nombre_archivo]
+        return Response(contenido_archivo, mimetype='application/octet-stream')
+
+
+
+    # @app.route('/recuperar_archivo', methods=['GET'])
+    # def recuperar_archivo():
+    #     # Convertir la lista archivos a bytes
+    #     contenido_archivo = bytes(archivos)
+
+    #     # Devolver el contenido de la lista archivos como parte del cuerpo de la respuesta HTTP
+    #     return Response(contenido_archivo, mimetype='application/octet-stream')
+
+
+    app.run(debug=True, host='0.0.0.0', port=port)
---
+++ server/server.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+import json
+
+
+app = Flask(__name__)
+
+data_nodes = []
+direccion_archivos_guardados = []
+
+@app.route('/register', methods=['POST'])
+def register_data_node():
+    data = request.json
+    host = data.get('host')
+    port = data.get('port')
+    capacidad = data.get('capacidad')
+
+    # Verificar si el DataNode ya esta registrado
+    for node in data_nodes:
+        if node['host'] == host and node['port'] == port:
+            return jsonify({'message': 'DataNode ya registrado'}), 200
+
+    # Si el DataNode no está registrado, agregarlo a la lista
+    data_nodes.append({'host': host, 'port': port, 'capacidadActual': capacidad})
+
+    print("Lista de DataNodes registrados:")
+    for node in data_nodes:
+        print(node)
+
+    return jsonify({'message': 'DataNode registrado correctamente'}), 200
+
+if __name__ == '__main__':
+    # Especifica el HOST y PORT deseado
+    host = '0.0.0.0'  # Por ejemplo
+    port = 5000  # Por ejemplo
+
+    @app.route('/opcionesDataNodes', methods=['POST'])
+    def buscar_dataNodes_disponibles():
+        # Ordenar la lista de DataNodes por 'capacidadActual' en orden descendente
+        data_nodes_ordenados = sorted(data_nodes, key=lambda x: x['capacidadActual'], reverse=True)
+        
+        # Seleccionar los dos primeros DataNodes con la mayor capacidad
+        data_nodes_top = data_nodes_ordenados[:2]
+        
+        # Devolver los dos DataNodes seleccionados como respuesta
+        return jsonify(data_nodes_top)
+
+    @app.route('/actualizarCapacidadDataNode', methods=['POST'])
+    def actualizar_capacidad_data_node():
+        data_actualizacion = request.json.get('data')
+        nuevo_host = data_actualizacion['host']
+        nuevo_port = data_actualizacion['port']
+        nueva_capacidad = data_actualizacion['nuevaCapacidad']
+        
+        for data_node in data_nodes:
+            if data_node['host'] == nuevo_host and data_node['port'] == nuevo_port:
+                data_node['capacidadActual'] = nueva_capacidad
+                print("Lista de DataNodes ahora:")
+                for node in data_nodes:
+                    print(node)
+                return 'Capacidad actualizada correctamente.', 200
+        
+       
+        
+        
+        
+        return 'No se encontró el DataNode especificado.', 404
+
+        
+
+    
+    @app.route('/guardar_ubicacion_archivo', methods=['POST'])
+    def guardar_ubicacion_archivo():
+        ubicacion_archivo = request.json.get('ubicacion')
+        nombre = ubicacion_archivo['nombre'] 
+        posicion = ubicacion_archivo['posicion'] 
+        host = ubicacion_archivo['host'] 
+        port = ubicacion_archivo['port'] 
+
+        direccion_archivos_guardados.append({"nombre": nombre, "posicion": posicion, "host": host, "port": port})
+        
+
+        return jsonify({'message': 'Ubicación del archivo guardada correctamente'}), 200
+
+    
+
+    @app.route('/recuperar_ubicacion_archivos', methods=['GET'])
+    def devolver_ubicacion_archivos():
+        # Convertir la lista a formato JSON
+        ubicacion_json = json.dumps(direccion_archivos_guardados)
+        
+        # Devolver la lista como parte de la respuesta HTTP
+        return ubicacion_json, 200
+
+    
+
+    # Ejecuta la aplicación Flask con el HOST y PORT especificado
+    app.run(debug=True, host=host, port=port)
